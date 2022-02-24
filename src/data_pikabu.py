6a390c2365f61b46d6fab759d226acceb251e083
dilyararimovna <dilyara.rimovna@gmail.com>
2022-02-24 13:01:43 +0300

    feat: consider one year only

--- src/data_pikabu.py
+++ src/data_pikabu.py
@@ -23,7 +23,7 @@ def get_dates(year_from, year_to=None):
         year_to = year_from
     dates = []
     for year in range(year_from, year_to + 1):
-        for _mo in range(1, 12 + 1):
+        for _mo in range(1, 2):
             mo = str(_mo)
             if len(mo) == 1:
                 mo = '0' + mo
